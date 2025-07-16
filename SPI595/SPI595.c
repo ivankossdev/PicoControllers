c2d8a7cdb74eebd782bac948cbafde52462bc329
ivan.koss <ivan.koss@yandex.ru>
2025-07-16 22:00:28 +0300

    spi595 not work

---
+++ SPI595/SPI595.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/spi.h"
+
+// SPI Defines
+// We are going to use SPI 0, and allocate it to the following GPIO pins
+// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
+#define SPI_PORT spi0
+#define PIN_MISO 16
+#define PIN_CS   17
+#define PIN_SCK  18 // 11 | SH_CP | Clock
+#define PIN_MOSI 19 // 14 | DS    | Data pin
+#define PIN_SS   20 // 12 | ST_CP | Storage Registet input
+
+// Функция для отправки данных через SPI
+void spi_write_byte(uint8_t data) {
+    gpio_put(PIN_CS, 0); // Захват данных
+    spi_write_blocking(SPI_PORT, &data, 1); // Отправка байта
+    gpio_put(PIN_CS, 1); // Освобождение данных
+    // Пауза для синхронизации (если необходимо)
+    sleep_us(1);
+}
+
+// Функция для установки значения на 595
+void set_595(uint8_t value) {
+    // Передача данных в регистр 595 через SPI
+    spi_write_byte(value);
+    // Управление выводом данных (строб)
+    gpio_put(PIN_SS, 0);
+    sleep_us(1);
+    gpio_put(PIN_SS, 1);
+    sleep_us(1);
+
+}
+
+
+int main()
+{
+    stdio_init_all();
+
+    // SPI initialisation. This example will use SPI at 1MHz.
+    spi_init(SPI_PORT, 1000*1000);
+    gpio_set_function(PIN_MISO, GPIO_FUNC_SPI);
+    gpio_set_function(PIN_CS,   GPIO_FUNC_SIO);
+    gpio_set_function(PIN_SCK,  GPIO_FUNC_SPI);
+    gpio_set_function(PIN_MOSI, GPIO_FUNC_SPI);
+    
+    // Chip select is active-low, so we'll initialise it to a driven-high state
+    gpio_set_dir(PIN_CS, GPIO_OUT);
+    gpio_put(PIN_CS, 1);
+    // For more examples of SPI use see https://github.com/raspberrypi/pico-examples/tree/master/spi
+
+    while (true) {
+        set_595(0x0f);
+        sleep_ms(1000);
+        set_595(0xf0);
+        sleep_ms(1000);
+    }
+}
